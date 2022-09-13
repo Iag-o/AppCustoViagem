30f4cb44bebd45cc151a072e89b55a49fb1546ea
Usuario <Usuario@CPD03-M01>
2022-09-13 12:27:45 -0300

    CriandoProjeto

---
+++ AppCustoViagemP/Model/Pedagio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AppCustoViagemP.Model
+{
+	public class Pedagio()
+	{
+		string _localizacao;
+		double _valor;
+
+		public string localizacao
+    {
+		get => _localizacao;
+        set
+        {
+			if (value == null)
+				throw new Exception("Informe a localização");
+
+			_localizacao = value;
+        }
+    }
+	public double valor
+    {
+		get => _valor;
+        set
+        {
+			if (value == 0)
+				throw new Exception("Informe o valor");
+
+			_valor = value;
+        }
+    }
+	}
+}
